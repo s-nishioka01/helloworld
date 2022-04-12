f86f72de8d68648f778ead1f295df491c2cc3ab3
s.n <s.n.@esunnoMacBook-Air.local>
2022-04-12 16:08:20 +0900

    add new file

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+package helloworld;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		for(int i = 1; i <= 10; i++) {
+			if(i == 10) {
+				System.out.println("Hello");
+			}else {
+				System.out.println("Hello World");
+			}
+		}
+	}
+}
