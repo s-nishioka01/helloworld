28edd6af01907677d56866d1787c2556f499531a
s.n <s.n.@esunnoMacBook-Air.local>
2022-04-14 22:25:46 +0900

    format code

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,10 +2,10 @@ package helloworld;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		for(int i = 1; i <= 10; i++) {
-			if(i == 10) {
+		for (int i = 1; i <= 10; i++) {
+			if (i == 10) {
 				System.out.println("Hello");
-			}else {
+			} else {
 				System.out.println("Hello World");
 			}
 		}
